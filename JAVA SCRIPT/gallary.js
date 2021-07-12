a7bd8b765456ee080c1dc08a625eed27bdeddb78
Wimalananda <vinukawi10@gmail.com>
2021-07-12 20:40:09 +0530

    gallary js

---
+++ JAVA SCRIPT/gallary.js
@@ -0,0 +1,32 @@
+function ChangeBcolor(){
+    var ColorOption= document.getElementById("BgbColor")[document.getElementById("BgbColor").selectedIndex].value;
+    if (ColorOption==="#34495E"){
+        document.body.style.backgroundColor="#34495E";
+    }
+    else if(ColorOption==="#7bc043"){
+        document.body.style.backgroundColor="#7bc043";
+    }
+    else if(ColorOption==="#be9b7b"){
+        document.body.style.backgroundColor="#be9b7b";
+    }
+    else if(ColorOption==="#afafaf"){
+        document.body.style.backgroundColor="#afafaf";
+    }
+    else if(ColorOption==="#ffaaa5"){
+        document.body.style.backgroundColor="#ffaaa5";
+    }
+    else if(ColorOption==="#f37735"){
+        document.body.style.backgroundColor="#f37735";
+    }
+    else if(ColorOption==="#8dbdff"){
+        document.body.style.backgroundColor="#8dbdff";
+    }
+    else if(ColorOption==="#fdfbfb"){
+        document.body.style.backgroundColor="#fdfbfb";
+    }
+}
+
+function ChangeTxColor(){
+    var ColorOption= document.getElementById("FontColor")[document.getElementById("FontColor").selectedIndex].value;
+    
+}
